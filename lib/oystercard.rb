2cbfc77bded236e0812ac226f6a47bf1c88bb4aa
David Papamichael <davidpapamichael@gmail.com>
2019-12-18 18:52:29 +0000

    Draft 2

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -15,7 +15,6 @@ class Oystercard
   def initialize
     @balance = 0
     @journey_history = []
-   
   end
 
   def top_up(money)
@@ -24,16 +23,33 @@ class Oystercard
   end
 
   def touch_in(station)
+    check_journey
+    @current_journey =  Journey.new
     fail "insufficent funds" if @balance < MIN
-    @current_journey = Journey.new
+    # @current_journey = Journey.new
     @current_journey.start_journey(station)
    
   end
 
   def touch_out(station)
+    check_journey
     deduct(MIN_CHARGE)
     @current_journey.finish_journey(station)
     @journey_history << @current_journey.journey
+  end
+
+  def check_journey
+
+    if @current_journey != nil
+      @current_journey.journey[:exit_station] = nil
+      @journey_history << @current_journey.journey
+    end
+
+    if @current_journey == nil
+      @current_journey = Journey.new
+      @current_journey.journey[:entry_station] == nil
+    end
+
   end
 
 
